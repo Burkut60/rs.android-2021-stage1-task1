6c6ec552944f3036f4170401448b07c4175cb3c6
= <=>
2021-05-25 22:01:18 +0300

    init: add task1 HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,28 @@
 package subtask1
 
 class HappyArray {
-
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        val mayArray = sadArray.toMutableList()
+        var happy: Boolean
+
+        do {
+            happy = false
+            for (i in 1 until mayArray.size - 1) {
+
+                val first = mayArray[i - 1]
+                val second = mayArray[i + 1]
+                val actual = mayArray[i]
+
+                if (first + second < actual) {
+                    mayArray.removeAt(i)
+                    happy = true
+                    break
+                }
+            }
+        } while (happy)
+
+        return mayArray.toIntArray()
     }
 }
